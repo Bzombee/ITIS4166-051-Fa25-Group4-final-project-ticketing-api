7799c99ba97088770166e77d85a866bbfc0002bc
Brian Zielinski <bzielins@charlotte.edu>
2025-11-13 11:47:47 -0500

    style: removed validateUpdateUser from imports

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { signUpHandler, logInHandler } from '../controllers/authController.js';
-import {validateUser, validateUpdateUser, validateRegisterUser} from '../middleware/userValidators.js';
+import {validateUser, validateRegisterUser} from '../middleware/userValidators.js';
 import loginLimiter from '../middleware/rateLimiter.js';
 
 const router = express.Router();
