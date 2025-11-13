656181d2528586055d813dad6729205ff5f16101
Jluu26 <jluu2@charlotte.edu>
2025-11-13 14:05:37 -0500

    Updated userValidators.js
    Exported: validateUpdateUser

--- src/middleware/userValidators.js
+++ src/middleware/userValidators.js
@@ -59,62 +59,62 @@ export const validateRegisterUser = [
 
 // Maybe not needed? Keeping it for now.
 
-// export const validateUpdateUser = [
-//   oneOf(
-//     [
-//       body('name').exists({ values: 'falsy' }),
-//       body('email').exists({ values: 'falsy' }),
-//       body('birthday').exists({ values: 'falsy' }),
-//       body('password').exists({ values: 'falsy' }),
-//     ],
-//     {
-//       message: 'At least one field (name, email, birthday, password) must be provided',
-//     },
-//   ),
+ export const validateUpdateUser = [
+   oneOf(
+     [
+       body('name').exists({ values: 'falsy' }),
+       body('email').exists({ values: 'falsy' }),
+       body('birthday').exists({ values: 'falsy' }),
+       body('password').exists({ values: 'falsy' }),
+     ],
+     {
+       message: 'At least one field (name, email, birthday, password) must be provided',
+     },
+   ),
 
-//   body('name')
-//     .optional()
-//     .trim()
-//     .isString()
-//     .withMessage('name must be a string')
-//     .bail(),
+   body('name')
+     .optional()
+     .trim()
+     .isString()
+     .withMessage('name must be a string')
+     .bail(),
 
-//   body('email')
-//     .optional()
-//     .trim()
-//     .isString()
-//     .withMessage('email must be a string')
-//     .bail()
-//     .isEmail()
-//     .withMessage('email is not valid')
-//     .normalizeEmail()
-//     .custom(async (value) => {
-//       if (await existsEmail(value)) {
-//         return Promise.reject(
-//           `The email ${value.toLowerCase()} is already in use.`,
-//         );
-//       }
-//       return true;
-//     }),
+   body('email')
+     .optional()
+     .trim()
+     .isString()
+     .withMessage('email must be a string')
+     .bail()
+     .isEmail()
+     .withMessage('email is not valid')
+     .normalizeEmail()
+     .custom(async (value) => {
+       if (await existsEmail(value)) {
+         return Promise.reject(
+           `The email ${value.toLowerCase()} is already in use.`,
+         );
+       }
+       return true;
+     }),
 
-//   body('birthday')
-//     .optional()
-//     .trim()
-//     .escape()
-//     .isDate({ format: 'YYYY-MM-DD', strictMode: true })
-//     .withMessage('Date must be in YYYY-MM-DD format and a valid date.'),
+   body('birthday')
+     .optional()
+     .trim()
+     .escape()
+     .isDate({ format: 'YYYY-MM-DD', strictMode: true })
+     .withMessage('Date must be in YYYY-MM-DD format and a valid date.'),
 
-//   body('password')
-//     .optional()
-//     .trim()
-//     .escape()
-//     .isLength({ min: 8, max: 64 })
-//     .withMessage(
-//       'password must contain at least 8 characters and at most 64 characters',
-//     ),
+   body('password')
+     .optional()
+     .trim()
+     .escape()
+     .isLength({ min: 8, max: 64 })
+     .withMessage(
+       'password must contain at least 8 characters and at most 64 characters',
+     ),
 
-//   handleValidationErrors,
-// ];
+   handleValidationErrors,
+ ];
 
 
 const validRoles = ["USER", "ADMIN", "ORGANIZER"]
