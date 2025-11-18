e5785b9bc40cdf7588ac2200bc4fb2499c4b086e
Brian Zielinski <bzielins@charlotte.edu>
2025-11-18 11:41:44 -0500

    refactor: routes use authenticate explicitly and initial delete endpoint

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -7,13 +7,15 @@ import {
   getMyOrdersHandler,
   cancelOrderHandler,
 } from '../controllers/orderController.js';
+import { authroizeRoles } from '../middleware/authorizeRoles.js';
 
 const router = express.Router();
 
-router.use(authenticate);
-router.post('/', validateOrder, createOrderHandler);
-router.get('/me', getMyOrdersHandler);
-router.get('/me/:id', getMyOrderByIdHandler);
-router.patch('/:id/status', cancelOrderHandler);
+// Implement delete
+router.post('/', authenticate, validateOrder, createOrderHandler);
+router.get('/me', authenticate, getMyOrdersHandler);
+router.get('/me/:id', authenticate, getMyOrderByIdHandler);
+router.patch('/:id/status', authenticate, cancelOrderHandler);
+router.delete('/:id', authenticate, authroizeRoles('ADMIN'))
 
 export default router;
