bc31a8da1da6799c1474e8b68004b28771186295
Brian Zielinski <bzielins@charlotte.edu>
2025-11-22 16:22:26 -0500

    feat: added authentication placeholders for routes

--- src/routes/ticketRoutes.js
+++ src/routes/ticketRoutes.js
@@ -3,17 +3,19 @@ import {
   validateTicket,
   validateTicketUpdate,
 } from '../middleware/ticketValidators.js';
+import { authenticate } from '../middleware/authenticate.js';
+import { authroizeRoles } from '../middleware/authorizeRoles.js';
 import { createTicketHandler, getForEventHandler, getTicketByIdHandler, updateTicketHandler, deleteTicketHandler } from '../controllers/ticketController.js';
 
 const router = express.Router();
 
-router.post('/:eventId/tickets', validateTicket, createTicketHandler);
 //router.get('/'); // implement get all tickets
-//router.get('/me') //implement tickets for logged in user
-router.get('/:eventId/tickets', getForEventHandler);
 router.get('/:id', getTicketByIdHandler);
-router.put('/:id', validateTicketUpdate, updateTicketHandler);
-//router.patch(':id/status'); //implement changing ticket status
-router.delete('/:id', deleteTicketHandler);
+//router.get('/me', authenticate) //implement tickets for logged in user
+router.get('/:eventId/tickets', getForEventHandler);
+router.post('/:eventId/tickets', authenticate, authroizeRoles('ADMIN', 'ORGANIZER'), validateTicket, createTicketHandler);
+router.put('/:id', authenticate, authroizeRoles('ADMIN', 'ORGANIZER'), validateTicketUpdate, updateTicketHandler);
+//router.patch(':id/status', authenticate, authroizeRoles('ADMIN', 'ORGANIZER')); //implement changing ticket status
+router.delete('/:id', authenticate, authroizeRoles('ADMIN', 'ORGANIZER'), deleteTicketHandler); //delete ticket checker, see if ticket not sold
 
 export default router;
